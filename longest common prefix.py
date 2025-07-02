018df2c0e69f0fc2ea1018834fc0fcf7a072ae0f
Vignesh <r.vignesh.rvrj@gmail.com>
2025-07-02 13:48:21 +0530

    LONGEST COMMON PREFIX

---
+++ longest common prefix.py
@@ -0,0 +1,20 @@
+def lcp_h(strs):
+  if len(strs) == 0:
+    return ""
+  prefix = strs[0]
+  for i in range(1,len(str)):
+    while str[i].find(prefix) != 0:
+      prefix == prefix [0:len(prefix)-1]
+      if prefix  == "" :
+        return ""
+  return prefix
+
+def lcp_v(strs):
+  if strs == NONE or len(strs) == 0:
+    return ""
+  for i in range(i,len(strs[0]):
+    c = str[0][i]
+    for j in range(i+1,len(strs)):
+      
+    
+  
